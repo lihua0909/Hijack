29dd58161c86cf1f34fbeefc3ecf59b46e804701
Administrator <Administrator@USER-20160613LQ>
2016-09-10 15:50:33 +0800

    Signed-off-by: Administrator <Administrator@USER-20160613LQ>

---
+++ src/com/group/internet/InternetHelper.java
@@ -0,0 +1,63 @@
+package com.group.internet;
+
+import java.io.OutputStream;
+
+import com.group.utils.PersonInfo;
+
+public class InternetHelper implements InternetFace {
+
+	@Override
+	public int Login(String username, String passward) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int Register(PersonInfo info) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sendText(String from, String to, String text) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sendText(String from, String to, OutputStream out) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sendText(String to, String text) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sendFile(String to, String path) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sendFile(String from, String to, String path) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sendFile(String to, OutputStream out) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public PersonInfo getPersonInfo(String who) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
