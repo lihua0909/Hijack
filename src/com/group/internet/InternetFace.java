0baa554abc0632855ee05d0bc9d9eda336628ad1
Liuben <1058320559@qq.com>
2016-09-10 14:57:22 +0800

    add internet

---
+++ src/com/group/internet/InternetFace.java
@@ -0,0 +1,68 @@
+package com.group.internet;
+
+import java.io.OutputStream;
+
+import com.group.utils.PersonInfo;
+
+/**
+ * 网络端的一些接口
+ * @author Administrator
+ *
+ */
+public interface InternetFace {
+	
+	/**
+	 * 登陆
+	 * @param username
+	 * @param passward
+	 * @return 1 为成功，0为失败
+	 */
+	public int Login(String username,String passward);
+	
+	/**
+	 * 注册
+	 * @param info
+	 * @return 1 为成功，0为失败
+	 */
+	public int Register(PersonInfo info);
+	
+	
+	/**
+	 *发送文本消息
+	 * @param from 发送方
+	 * @param to	接收方
+	 * @param text	文本
+	 * @return 1 为成功，0为失败
+	 */
+	public int sendText(String from,String to,String text);
+	
+	public int sendText(String from,String to,OutputStream out);
+	
+	/**
+	 *发送文本消息
+	 * @param to	接收方
+	 * @param text	文本
+	 * @return  1 为成功，0为失败
+	 */
+	public int sendText(String to,String text);
+	
+	/**
+	 * 发送文件
+	 * @param to
+	 * @param path
+	 * @return
+	 */
+	public int sendFile(String to,String path);
+	
+	public int sendFile(String from ,String to,String path);
+	
+	public int sendFile(String to,OutputStream out);
+	
+	/**
+	 * 
+	 * @param who 
+	 * @return 返回个人信息
+	 */
+	public PersonInfo getPersonInfo(String who);
+	
+}
---
+++ src/com/group/utils/PersonInfo.java
@@ -0,0 +1,12 @@
+package com.group.utils;
+
+/**
+ *  person information
+ *  
+ * @author Administrator
+ *
+ */
+public class PersonInfo {
+	
+
+}
